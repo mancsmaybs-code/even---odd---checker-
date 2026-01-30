c500f29ae8a1372bd60a1ea4ee95c5fafcbfb856
mancsmaybs-code <mancsmaybs@gmail.com>
2026-01-30 08:28:21 +0800

    Added input validation and improved output message

--- even_odd.cpp
+++ even_odd.cpp
@@ -1,17 +1,21 @@
-‎#include <iostream>
-‎using namespace std;
-‎
-‎int main() {
-‎    int number;
-‎
-‎    cout << "Enter a number: ";
-‎    cin >> number;
-‎
-‎    if (number % 2 == 0) {
-‎        cout << "The number is even." << endl;
-‎    } else {
-‎        cout << "The number is odd." << endl;
-‎    }
-‎
-‎    return 0;
-‎}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+
+    if (!(cin >> number)) {
+        cout << "Invalid input. Please enter an integer." << endl;
+        return 0;
+    }
+
+    if (number % 2 == 0) {
+        cout << number << " is an even number." << endl;
+    } else {
+        cout << number << " is an odd number." << endl;
+    }
+
+    return 0;
+}
